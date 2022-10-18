c8fa45577de52bcd99734f2d739f8fd15843dc96
Jan.Choutka <cho0178@vsb.cz>
2022-10-18 10:36:40 +0200

    add cmp and dac examples CHO0178

---
+++ mkl25z4_temp/src/demo/CHO0178/cmp_PIT_trig_E.c
@@ -0,0 +1,113 @@
+#include "wdog.h"
+#include "littleHelper.h"
+#include "MKL25Z4.h"
+
+#include "soc_def.h"
+#include "res_alloc.h"
+
+#include "wdog.h"
+#include "led.h"
+#include "sin_lut.h"
+
+// compare trig
+/*
+DAC
+	DAT0L
+		DATA0			lower data pro pøevod
+	DAT0H
+		DATA1			higher data pro pøevod
+	C0
+		DACEN			povolí použití 12bit DAC
+CMP
+	DACCR
+		DACEN			povolí napájení vnitøního DAC
+		VOSEL			voltage select
+	MUXCR
+		PSEL			multiplex to positive input of CMP
+		MSEL			multiplex to negative input of CMP
+	SRC
+		IER				enable flag raising
+		CFR				flag interruptu
+	CR1
+		OPE				enable output
+		EN				enable power for CMP
+*/
+
+#define PIT_EXPT_PRI		2u
+#define CMP_EXPT_PRI		2u
+#define PIT0_MOD			3000ul
+#define DAC0_DAT0			(*((volatile uint16_t *) 0x4003F000ul))
+#define CMP_EXPT_CNT_MOD	50u
+
+
+uint16_t sin_lut_ind, cmp_expt_cnt, disp_cnt;
+const uint16_t sin_lut[SIN_LUT_DATA_LEN] = {SIN_LUT_DATA};
+
+void setupNVICandPIT();
+
+
+int main(void)
+{
+	wdog_init(WDOG_CONF_LPOCLK_1024_CYCLES);
+	led_init();
+	setupNVICandPIT();
+
+	// povolte použití 12bit DAC
+
+
+	// pøepnìte multiplex v periferii port tak, aby pøesmìroval signál z BNC konektoru J15 na výstup periferie CMP0
+
+	// pøepnìte multiplex v periferii port tak, aby pøesmìroval signál z výstupu DAC na BNC konektor
+
+	// nastavte priferii CMP tak aby výstupní hodnota vosel byla 0x20u
+
+	// nastavte vstupní multiplexory na kladný vstup periferii DAC0 a záporný vstup signal DAC periferie CMP
+
+	// povol flag raising v periferii CMP
+
+	// povol výstup a napájení periferie CMP
+
+
+	while (1) {
+		wdog_refresh();
+	}
+
+	return 0;
+}
+
+
+void setupNVICandPIT()
+{
+	NVIC_SetPriority(PIT_IRQn, PIT_EXPT_PRI);
+	NVIC_EnableIRQ(PIT_IRQn);
+
+	NVIC_SetPriority(CMP0_IRQn, PIT_EXPT_PRI);
+	NVIC_EnableIRQ(CMP0_IRQn);
+
+	PIT->MCR = PIT_MCR_FRZ_MASK;
+	PIT->CHANNEL[0u].LDVAL = PIT0_MOD - 1u;
+	PIT->CHANNEL[0u].TCTRL = PIT_TCTRL_TIE_MASK | PIT_TCTRL_TEN_MASK;
+
+}
+
+
+void __attribute__ ((interrupt)) PIT_IRQHandler(void)
+{
+	PIT->CHANNEL[0u].TFLG = PIT_TFLG_TIF_MASK;
+
+	DAC0_DAT0 = sin_lut[sin_lut_ind];
+
+	sin_lut_ind++;
+	if (sin_lut_ind == SIN_LUT_DATA_LEN) {
+		sin_lut_ind = 0;
+	}
+}
+
+void __attribute__ ((interrupt)) CMP0_IRQHandler(void)
+{
+	// proveïte vynulování vyhozeného flagu (dejte si pozor aby jste nevynulovali povolení flagù)
+
+
+	// èítej poèet vyvolaných interruptù a každé 50 volání zvìtši výstup na diodì o 1
+
+}
---
+++ mkl25z4_temp/src/demo/CHO0178/dac_PIT_trig_F.c
@@ -0,0 +1,66 @@
+#include "wdog.h"
+#include "littleHelper.h"
+#include "MKL25Z4.h"
+#include "sin_lut.h"
+/*
+DAC
+	DAT0L
+		DATA0			lower data pro pøevod
+	DAT0H
+		DATA1			higher data pro pøevod
+	C0
+		DACEN			povolí použití 12bit DAC
+
+
+*/
+#define PIT_EXPT_PRI		2u
+#define PIT0_MOD			3000ul
+
+
+uint16_t sin_lut_ind;
+const uint16_t sin_lut[SIN_LUT_DATA_LEN] = {SIN_LUT_DATA};
+
+void setupNVICandPIT();
+
+int main(void)
+{
+	wdog_init(WDOG_CONF_LPOCLK_1024_CYCLES);
+	// povolte použití 12bit DAC
+	DAC0->C0 |= 1<<DAC_C0_DACEN_SHIFT;
+
+	setupNVICandPIT();
+
+
+	while (1) {
+		wdog_refresh();
+	}
+
+	return 0;
+}
+
+
+void setupNVICandPIT()
+{
+	NVIC_SetPriority(PIT_IRQn, PIT_EXPT_PRI);
+	NVIC_EnableIRQ(PIT_IRQn);
+
+	PIT->MCR = PIT_MCR_FRZ_MASK;
+	PIT->CHANNEL[0u].LDVAL = PIT0_MOD - 1u;
+	PIT->CHANNEL[0u].TCTRL = PIT_TCTRL_TIE_MASK | PIT_TCTRL_TEN_MASK;
+}
+
+void __attribute__ ((interrupt)) PIT_IRQHandler(void)
+{
+	PIT->CHANNEL[0u].TFLG = PIT_TFLG_TIF_MASK;
+
+	// vložte hodnoty z sin_lut do registrù DAT0L, DAT0H
+	DAC0->DAT[0].DATH = sin_lut[sin_lut_ind]>>8;
+	DAC0->DAT[0].DATL = sin_lut[sin_lut_ind];
+
+	// inkrementujte index ukazující na pøíští hodnotu použitou po DAC
+	sin_lut_ind++;
+	// pøi pøesáhnutí indexu hodnoty SIN_LUT_DATA_LEN vynuluj index
+	if (sin_lut_ind == SIN_LUT_DATA_LEN) {
+		sin_lut_ind = 0;
+	}
+}
