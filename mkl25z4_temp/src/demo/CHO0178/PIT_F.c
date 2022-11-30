15dc6a1a7587670abeb79c75113be15c8b7692e2
Jan.Choutka <cho0178@vsb.cz>
2022-11-30 13:44:08 +0100

    fix of mistake in pit

--- mkl25z4_temp/src/demo/CHO0178/PIT_F.c
+++ mkl25z4_temp/src/demo/CHO0178/PIT_F.c
@@ -21,26 +21,26 @@ int main(void)
 {
 	wdog_init(WDOG_CONF_LPOCLK_1024_CYCLES);
 	//(0) inicializujte diody
-	led_rgb_init();
-	//(0) nastavte zastavení v debug rezimu a povolte periferii PIT
-	PIT->MCR |= PIT_MCR_FRZ(1);
-	PIT->MCR &= ~PIT_MCR_MDIS_MASK;
+	led_init();
+	//(0) nastavte zastavení PIT v debug rezimu a povolte periferii PIT
+	PIT->MCR = PIT_MCR_FRZ_MASK;
 	//(0) nastavte modulo pro obnovovani na 1.5s (LDVAL, busFrequency 24MHz)
-	PIT->CHANNEL[0].LDVAL = 36000000;
+	PIT->CHANNEL[0].LDVAL = 36000000ul;
 	//(0) povolte casovac PIT a generovani interruptu (TCTRL)
-	PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TEN_MASK & PIT_TCTRL_TIE_MASK;
+	PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TEN_MASK | PIT_TCTRL_TIE_MASK;
+	PIT->CHANNEL[0].TCTRL &= ~PIT_TCTRL_CHN_MASK;
 	//(0) povolte v prijem preruseni v periferii NVIC
 	NVIC_EnableIRQ(PIT_IRQn);
 	NVIC_SetPriority(PIT_IRQn,2);
 	//(1) nastavte druhý kanál na periodu 2s
-	PIT->CHANNEL[1].LDVAL = 72000000;
-	PIT->CHANNEL[1].TCTRL |= PIT_TCTRL_TEN_MASK & PIT_TCTRL_TIE_MASK;
+	PIT->CHANNEL[1].LDVAL = 48000000ul;
+	PIT->CHANNEL[1].TCTRL |= PIT_TCTRL_TEN_MASK | PIT_TCTRL_TIE_MASK;
+	PIT->CHANNEL[1].TCTRL = ~PIT_TCTRL_CHN_MASK;
 	//(2) nastavte èas systicku na 0.5s
-	SysTick->LOAD = 1500000 - 1u;
-	SysTick->VAL = 0ul;
-	SysTick->CTRL = SysTick_CTRL_ENABLE_Msk;
+//	SysTick->LOAD = 1500000 - 1u;
+//	SysTick->VAL = 0ul;
+//	SysTick->CTRL = SysTick_CTRL_ENABLE_Msk;
 	while (1) {
-
 		wdog_refresh();
 	}
 
@@ -54,15 +54,16 @@ int main(void)
 
 void PIT_IRQHandler()
 {
+	if(PIT->CHANNEL[1].TFLG & PIT_TFLG_TIF_MASK){
+		PIT->CHANNEL[1].TFLG = PIT_TFLG_TIF_MASK;
+		led_toggle(LED_ID2);
+	}
 	if(PIT->CHANNEL[0].TFLG & PIT_TFLG_TIF_MASK){
-		PIT->CHANNEL[0].TFLG &= ~PIT_TFLG_TIF_MASK;
+		PIT->CHANNEL[0].TFLG = PIT_TFLG_TIF_MASK;
 		led_toggle(LED_ID1);
 	}
 
-	if(PIT->CHANNEL[1].TFLG & PIT_TFLG_TIF_MASK){
-		PIT->CHANNEL[1].TFLG &= ~PIT_TFLG_TIF_MASK;
-		led_toggle(LED_ID2);
-	}
+
 }
 
 //(2) vytvoøte handler pro pøerušení ze systicku a pøepínejte ledku 2
